7c4108f616c3f121136c2d8bdeb4b614b9996d95
amanullha <aullah060@gmail.com>
2023-07-07 00:48:14 +0600

    update vercel config

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ const port = process.env.APP_PORT;
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to folder tree view");
 });
+app.get("/test", (req: Request, res: Response) => {
+  res.send({
+    data: "hello! from backend",
+  });
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
